f730c96b669d439e32bdbe7468bcc6d07d013a67
Giovanni Souza <GSMartins999>
2023-09-24 19:31:35 -0300

    comentariofeito

--- src/componentes/Comentarios/Comentarios.js
+++ src/componentes/Comentarios/Comentarios.js
@@ -1,22 +1,99 @@
-import styles from "./Comentarios.module.css"
+import { useState } from "react";
+import {
+  InputContainer,
+  ListaContainer,
+  ListaTarefasContainer,
+  Tarefa,
+  TaskInput,
+  AddTaskButton,
+  RemoveButton,
+  LinhaHorizontal,
+  Container,
+  ListaMostrada
+} from "./styled";
+import bin from "./../../img/bin.png";
 
+const listaDeTarefasInicial = [
+  {
+    titulo: "Eu adorei as camisetas, vou comprar mais."
+  },
+  {
+    titulo: "Adorei o site, ficou incrível!!"
+  },
+  {
+    titulo: "Adorei o site, ficou incrível!!"
+  }
+];
 
-function Comentarios(){
+export function ListaTarefas() {
+  const [lista, setLista] = useState(listaDeTarefasInicial);
+  const [novaTarefa, setNovaTarefa] = useState({ titulo: ""});
+  const [removed, setRemoved] = useState([]);
+  const onChangeTarefa = (event) => { //função que adiciona a tarefa
+    const tarefa = {
+      titulo: event.target.value,
+    };
+    setNovaTarefa(tarefa);
+  };
 
-    return(
+  const adicionaTarefa = () => {
+    const novaLista = [...lista, novaTarefa];
+    setLista(novaLista);
+    setNovaTarefa({ titulo: ""});
+  };
 
-        <>
-        <div className={styles.container}>
-            <div>
-                <input 
-                    placeholder="Comentários"
-                    className={styles.Comentarios}
-                />
-            </div>
-        </div>
+  const enter = (event) => { //função do enter
+    if(event.key === 'Enter'){
 
-        </>
-    );
+      const novaTarefa = {
+        titulo: event.target.value
+      };
+        setLista([...lista, novaTarefa]);
+        console.log(novaTarefa);
+        setNovaTarefa({ titulo: "" });
+    }
+  }
+
+  const removeTarefa = (tarefaParaRemover) => { //função para remover
+    const listaFiltrada = lista.filter(tarefa => tarefa.titulo !== tarefaParaRemover.titulo);
+
+    setRemoved([...removed, tarefaParaRemover]);
+    setLista(listaFiltrada);
+  };
+
+  return (
+    <Container>
+    <ListaTarefasContainer>
+      <InputContainer>
+        <TaskInput
+          placeholder="Escreva aqui:"
+          value={novaTarefa.titulo}
+          onChange={onChangeTarefa}
+          onKeyDown={enter}
+        />
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
+      </InputContainer>
+      <ListaContainer>
+        <ListaMostrada>
+          <h2>Adicione Aqui seu comentário:</h2>
+          {lista.map((tarefa, index) => {
+            return (
+              <Tarefa key={index}>
+                <p>{tarefa.titulo}</p>
+                <RemoveButton onClick={() => removeTarefa(tarefa)}>
+                  <img src={bin} alt="" width="16px" />
+                </RemoveButton>
+              </Tarefa>
+            );
+          })}
+        </ListaMostrada>
+
+      </ListaContainer>
+      <LinhaHorizontal />
+    </ListaTarefasContainer>
+    </Container>
+  );
 }
 
-export default Comentarios;
+
+export default ListaTarefas;
